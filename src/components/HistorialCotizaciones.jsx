06095027c380cc6a7b8c893a42f3f624906a2b93
sametxcr <samuel.munoz.m12@gmail.com>
2026-07-02 09:59:58 -0400

    Update HistorialCotizaciones.jsx

--- src/components/HistorialCotizaciones.jsx
+++ src/components/HistorialCotizaciones.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { abrirVistaCotizacion } from '../App';
 
-const API_URL = 'http://localhost:3001/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function HistorialCotizaciones({
   setTabActivo,
