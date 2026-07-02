7bf0cac385903516db1b60858d185f01573d8ba8
sametxcr <samuel.munoz.m12@gmail.com>
2026-07-02 10:05:52 -0400

    Update ProductForm.jsx

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -252,7 +252,7 @@ const handleSubmit = async (e) => {
       const formData = new FormData();
       formData.append('foto', archivoFoto);
       
-      const res = await fetch(`${API_URL}/productos/${sku}/foto`, {
+      const res = await fetch(`${API_URL}/productos/${skuFinal}/foto`
         method: 'POST',
         body: formData
       });
